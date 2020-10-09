b2f7544b4d038a13a23f3ab10ed20da0f1442c19
furious-monkey <daniel.j.hahn1105@gmail.com>
2020-10-09 00:44:54 +0000

    When we submit we create a new div on the entries section an put the textbox value in it.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const entryForm = document.getElementById('entryForm');
+const entryForm = document.getElementById('entryForm')
+const entriesSection = document.getElementById('entries')
+const entryTextbox = document.getElementsByClassName('entry-textbox')
 
-//function to avoid refresh when submiting the form.
 function addEntryToDom(event) {
-    event.preventDefault();
+    event.preventDefault() // to avoid refresh when submiting the form.
+    const entryDiv = document.createElement("div")
+    entryDiv.className = 'single-entry' //asign classname to the new div element.
+    entryDiv.innerText = entryTextbox[0].value //because getElementsByClassName returns a "array-like" object we need to look at the first index of it to get the value.
+    entriesSection.append(entryDiv)
 }
 
 //listening for the submit and calling function to prevent refresh.
