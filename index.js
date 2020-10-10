cfd28913512d8dd02e4982fcfc661ef2aaa15063
furious-monkey <daniel.j.hahn1105@gmail.com>
2020-10-10 22:33:43 +0000

    When submiting, the value of entryTextbox gets reseted. some modificatins on the CSS files for #entries

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function addEntryToDom(event) {
     entryDiv.className = 'single-entry' //asign classname to the new div element.
     entryDiv.innerText = entryTextbox[0].value //because getElementsByClassName returns a "array-like" object we need to look at the first index of it to get the value.
     entriesSection.append(entryDiv)
+    entryTextbox[0].value = ''
 }
 
 //listening for the submit and calling function to prevent refresh.
